794495fab523baa93d022db46747590092948cdf
Nack <qwee6333@gmail.com>
2015-03-31 19:52:14 +0800

    Initial commit

---
+++ app/src/main/java/idv/nack/intenttest/SecondPage.java
@@ -0,0 +1,19 @@
+package idv.nack.intenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 202 on 2015/3/31.
+ */
+public class SecondPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondtwo);
+
+
+
+    }
+}
